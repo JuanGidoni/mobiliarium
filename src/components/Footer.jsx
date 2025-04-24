8241607b4906a51b2341d8010770fa31a28800ed
su4nsi <miguelperatorres@gmail.com>
2025-04-24 12:57:30 +0200

    feat: add header component with styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export function Footer() {
+  return (
+    <footer className="footer">
+            <p>&copy; 2025 Capgemini. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
