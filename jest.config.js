920920e629b85824ec217e4b117a08e06149681f
juani.dev <juan.gidoni@gmail.com>
2025-04-24 14:18:13 +0000

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,5 @@ export default {
   moduleNameMapper: {
     "^.+\\.(css|scss)$": "identity-obj-proxy",
   },
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testMatch: ["**/src/**/*.test.{js,jsx}"],
 };
