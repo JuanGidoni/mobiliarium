6534d2d0214c0f7632f61e794369217b12c22a7b
mariasannino <115103973+mariasannino@users.noreply.github.com>
2025-04-24 13:31:46 +0200

    creation navigation component and adjustments (#23)
    Co-authored-by: juani.dev <juan.gidoni@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,10 @@
-import { Link } from "react-router-dom";
-
+import Navigation from "./Navigation.jsx";
 export function Header() {
   return (
     <header className="main-header">
       <div className="container">
-        <h1 className="title">Perritex</h1>
-        <nav>
-          <ul className="nav-list">
-            <li>
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link" to="/categorias">
-                Categorías
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <h1 className="title">Nombre de la app</h1>
+        <Navigation />
       </div>
     </header>
   );
