17068983f7f8f6842912b788d0846e6375253821
juani.dev <juan.gidoni@gmail.com>
2025-04-25 10:42:46 +0000

    feat: add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer";
+import Button from "./components/button/Button";
 
 function App() {
   return (
     <>
       <Header />
       <Footer />
+      <Button text="Test 123" />
     </>
   );
 }
