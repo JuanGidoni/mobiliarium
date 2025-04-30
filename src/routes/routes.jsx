ffd6adeaf85e2be64993e293d8af6136cb1f9d17
su4nsi <miguelperatorres@gmail.com>
2025-04-30 11:36:54 +0200

    chore: add structure categories page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,8 @@
 import ErrorPage from "../components/error/ErrorPage";
 import Layout from "../layouts/Layout";
 import HomePage from "../pages/home/HomePage";
+import CategoriesPage from "../pages/categories/CategoriesPage";
+import CategoryDetailPage from "../pages/categories//categorydetails/CategoryDetailPage";
 
 export const routes = [
   {
@@ -8,5 +10,21 @@ export const routes = [
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [{ path: "", element: <HomePage /> }],
+  },
+  {
+    path: "/categories",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "", element: <CategoriesPage /> },
+    ],
+  },
+  {
+    path: "/categories/:category",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "", element: <CategoryDetailPage /> }, 
+    ],
   },
 ];
